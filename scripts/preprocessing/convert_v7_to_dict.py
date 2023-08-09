bd42e8654273e7365f04524ae7a0cc5a9dc87972
Jacob Yeung <jacobyeung@berkeley.edu>
2023-08-09 17:42:28 -0400

    convert .mat v7 to a python dictionary for easier use in python. saved as .pkl

---
+++ scripts/preprocessing/convert_v7_to_dict.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.io import loadmat
+import os, glob
+
+file_names = glob.glob("OFC_data/v7*.mat")
+print(file_names, "hi")
+
+
+for i in range(len(file_names)):
+    fn = file_names[i]
+    print(fn)
+    mat = loadmat(fn)
+    var_names = mat["mat_data"].dtype.names
+    dat = mat["mat_data"][0][0]
+    data_dict = {var_names[i]: dat[i] for i in range(len(var_names))}
+
+    # Squeeze extraneous dimensions
+    for k in data_dict.keys():
+        curr = data_dict[k]
+        data_dict[k] = np.squeeze(curr)
+
+    # Turn names into lists
+    for k in data_dict.keys():
+        if data_dict[k].dtype == object:
+            curr = data_dict[k]
+            curr = np.concatenate([*np.squeeze(curr)])
+            data_dict[k] = curr
+
+    # selectVerifiedAnatomy is a list of 4 strings
+    try:
+        data_dict["selectVerifiedAnatomy"] = np.array(
+            [
+                np.concatenate(alpha)
+                for alpha in data_dict["selectVerifiedAnatomy"].reshape(-1, 4)
+            ]
+        )
+    except ValueError:
+        print("selectVerifiedAnatomy is a list of  strings for EC155")
+        print(data_dict["selectVerifiedAnatomy"])
+
+    print(os.path.basename(fn).split(".")[0])
+
+    with open(f"OFC_data/{os.path.basename(fn).split('.')[0]}.pkl", "wb") as f:
+        np.save(f, data_dict)
