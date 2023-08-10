65c7ecc01a0b8456c16189c354e437c3d2c87661
Jacob Yeung <jacobyeung@berkeley.edu>
2023-08-10 15:32:22 -0400

    unsure why can't loop over creating spectra and need to process each one individually

---
+++ rao_scripts/Scripts_iEEG/JY_DuringContinuousStim_TimeAveragedSpectra.m
@@ -0,0 +1,93 @@
+%% KS_DuringContinuousStim_TimeAveragedSpectra.m
+%
+% Use preprocessed data from during continuous stimulation to calculate
+% spectra; average spectra before stimulation, during stimulation, and
+% after stimulation
+%
+% Author: Kristin Sellers, 2017-2018
+%%
+close all
+clear all
+clc
+%%
+directory_path = '../../OFC_data';
+% Load preprocessed stim data
+
+mat_files = dir(fullfile(directory_path, ['EC' '*.mat']));
+disp(mat_files)
+
+
+for file_idx = 1:numel(mat_files)
+
+    stimFileName = mat_files(file_idx).name;
+    stimFilePathECOG = mat_files(file_idx).folder;
+    patientID = stimFileName(1:5);
+    disp([stimFilePathECOG stimFileName]);
+    split_name = split(stimFileName, '_');
+
+    matchingIndices = cellfun(@(s) contains(s, 'mA'), split_name);
+    if isempty(matchingIndices)
+        close all;
+        continue;
+    end
+    currentStim =  split_name(matchingIndices);
+    
+    currentStim = currentStim{1};
+
+    rest_of_name = strjoin(split_name(3:end), '_');
+
+
+    % Check if the file name contains "v7"
+    if contains(stimFileName, 'v7')
+        disp(['Skipping file: ', stimFileName]);
+        continue; % Skip processing this file
+    end
+
+    if exist([stimFilePathECOG 'v7_' patientID '_TimeAveragedSpectraPower_' currentStim '.mat'], 'file') == 2
+        disp(['Finished file: ', stimFileName]);
+        continue;
+    end
+    
+%         [stimFileName, stimFilePathECOG] = uigetfile('*preprocessed*FreqBands*.mat','Select preprocessed stim ECOG data');
+    load([stimFilePathECOG '/' stimFileName])
+    
+    
+    
+    % Goes channel by channel, not at all at once
+    for iChan = 1:numVerifiedChans
+        display(['Hilbert Stim Data: Chan ' num2str(iChan) ' of ' num2str(numVerifiedChans)])
+        clear tempHilbertFilter
+        [tempHilbertFilter,freqs] = KSedits_processingHilbertTransform_filterbankGUI(preprocessedStimData(iChan,:),dsFs,[4 80]);
+        hilbertAA_stim(iChan,:,:) = squeeze(tempHilbertFilter.envData)';
+    end
+    power_stim = hilbertAA_stim.^2;
+    
+    %%
+    % Time-averaged spectrum for before, during, and after stimulation
+    stimStartSample = floor((plotStimStart/Fs)*dsFs);
+    stimEndSample = floor((plotStimEnd/Fs)*dsFs);
+    
+    %% Before Stim
+    beforeStim = power_stim(:,:,1:stimStartSample - 1);
+    
+    % Average across time
+    meanBeforeStim = squeeze(mean(beforeStim,3));
+    %% During Stim
+    duringStim = power_stim(:,:,stimStartSample:stimEndSample);
+    
+    % Average across time
+    meanDuringStim = squeeze(mean(duringStim,3));
+    %% After Stim
+    afterStim = power_stim(:,:,stimEndSample + 1:end);
+    
+    % Average across time
+    meanAfterStim = squeeze(mean(afterStim,3));
+    
+    %%
+    saveFileName = [stimFilePathECOG 'v7_' patientID '_TimeAveragedSpectraPower_' rest_of_name];
+    save(saveFileName, 'power_stim', 'meanBeforeStim','meanDuringStim','meanAfterStim',...
+        'freqs','finalVerifiedRegions','finalVerifiedChanNames','regionNames',...
+        'stimStartSample','stimEndSample','dsFs','Fs','numVerifiedChans','-v7')
+    close all
+    break;
+end
